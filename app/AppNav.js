357455212857addf54c2fe0244d98592a3b4e58a
jimmyliou <jimmyliou777@gmail.com>
2017-06-23 17:56:27 +0800

    C6

--- app/AppNav.js
+++ app/AppNav.js
@@ -1,44 +1,42 @@
-import React, {
-	Component
-} from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 
 class AppNav extends Component {
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this);
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
 
-	}
+    }
 
-	//before render
-	componentWillMount() {
-		console.log('componentWillMount');
+    //before render
+    componentWillMount() {
+        console.log('componentWillMount');
 
-	}
+    }
 
-	//after render
-	componentDidMount() {
-		console.log('componentDidMount');
+    //after render
+    componentDidMount() {
+        console.log('componentDidMount');
 
-	}
+    }
 
-	handleClick(e) {
-			if (this.props.menuState) {
-				this.props.onTodoRemove(0);
-			} else {
-				this.props.onTodoAdd(this.props.height);
-			}
+    handleClick(e) {
+        if (this.props.menuState) {
+            this.props.onTodoRemove(0);
+        } else {
+            this.props.onTodoAdd(this.props.height);
+        }
 
-		}
-		//render html elements
-	render() {
-		return (<div>
+    }
+    //render html elements
+    render() {
+        return (<div>
          <AppBar
-           title="JimmyProject"   
-           onLeftIconButtonTouchTap={this.handleClick}
-         />
+            title="Jimmy Blog"
+            onLeftIconButtonTouchTap={this.handleClick}
+            />
         </div>);
-	}
+    }
 }
 
 
--- app/AppRedux.js
+++ app/AppRedux.js
@@ -1,26 +1,13 @@
-import React, {
-	Component
-} from 'react';
-import {
-	bindActionCreators
-} from 'redux';
-import {
-	connect
-} from 'react-redux';
-import {
-	removeTodo,
-	addTodo
-} from './actions/menuAction';
-import {
-	Unmount,
-	Didmount,
-} from './actions/canvasAction';
-import {
-	browserHistory,
-	Link
-} from 'react-router';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { removeTodo, addTodo } from './actions/menuAction';
+import { Unmount, Didmount, } from './actions/canvasAction';
+import { browserHistory, Link } from 'react-router';
 
 import './RWD.css';
+import './css/flat-ui/dist/css/flat-ui.css';
+import './css/flat-ui/docs/assets/css/demo.css';
 
 import classNames from 'classnames';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -41,108 +28,110 @@ import loadingImg from './images/teddy-bear-loading-gif-5479.gif';
 injectTapEventPlugin();
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this);
-		this.swiper = {};
-		this._innerHeight = 0;
-		this.state = {
-			height: 0,
-			loadingState: 'none',
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.swiper = {};
+        this._innerHeight = 0;
+        this.state = {
+            height: 0,
+            loadingState: 'none',
+        }
+    }
 
-	handleClick(e, path) {
-		this.setState({
-			loadingState: 'block'
-		});
-		this.props.actions.removeClass(0);
-		setTimeout(() => {
-			browserHistory.push(path);
-			this.setState({
-				loadingState: 'none'
-			})
-		}, 500)
+    handleClick(e, path) {
+        this.setState({
+            loadingState: 'block'
+        });
+        this.props.actions.removeClass(0);
+        setTimeout(() => {
+            browserHistory.push(path);
+            this.setState({
+                loadingState: 'none'
+            })
+        }, 500)
 
-		if (path !== '/canvas') {
-			this.props.actions.Unmount();
-		} else {
-			this.props.actions.Didmount();
-		}
-	}
+        if (path !== '/canvas') {
+            this.props.actions.Unmount();
+        } else {
+            this.props.actions.Didmount();
+        }
+    }
 
-	//before render
-	componentWillMount() {
-		console.log('componentWillMount');
+    //before render
+    componentWillMount() {
+        console.log('componentWillMount');
 
-	}
+    }
 
-	//after render
-	componentDidMount() {
-		this.content = this.refs.content;
-		this.leftMenu = this.refs.leftMenu;
-		this._innerHeight = this.content.offsetHeight;
-		//this.swiper.intro = new Swiper('.swiper-container');
-		this.setState({
-			height: this.content.offsetHeight
-		});
-		//console.log(`this.state.height:${this.state.height}`);
-	}
+    //after render
+    componentDidMount() {
+        this.content = this.refs.content;
+        this.leftMenu = this.refs.leftMenu;
+        this._innerHeight = this.content.offsetHeight;
+        //this.swiper.intro = new Swiper('.swiper-container');
+        this.setState({
+            height: this.content.offsetHeight
+        });
+    //console.log(`this.state.height:${this.state.height}`);
+    }
 
-	render() {
-		let liClasses = classNames({
-			'leftMenu': true,
-			'navbar_collapse': true,
-			'in': this.props.inClass
-		});
-		let loadingStyle = {
-			backgroundColor: '#000',
-			width: '100%',
-			height: '100%',
-			opacity: 0.5,
-			position: 'absolute',
-			left: 0,
-			top: 0,
-			zIndex: 1000,
-			display: this.state.loadingState,
-		};
-		let LoadImage = {
-			position: 'absolute',
-			left: '25%',
-			top: '10%',
-			zIndex: 1000,
-			display: this.state.loadingState,
-		};
-		//console.log(`this.state.height-render:${this.state.height}`);
-		//傳遞dispatch作為props
-		// const {
-		//   dispatch
-		// } = this.props;
-		return (
-			<div>
+    render() {
+        let liClasses = classNames({
+            'leftMenu': true,
+            'navbar_collapse': true,
+            'in': this.props.inClass
+        });
+        let loadingStyle = {
+            backgroundColor: '#000',
+            width: '100%',
+            height: '100%',
+            opacity: 0.5,
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            zIndex: 1000,
+            display: this.state.loadingState,
+        };
+        let LoadImage = {
+            position: 'absolute',
+            left: '25%',
+            top: '10%',
+            zIndex: 1000,
+            display: this.state.loadingState,
+        };
+        //console.log(`this.state.height-render:${this.state.height}`);
+        //傳遞dispatch作為props
+        // const {
+        //   dispatch
+        // } = this.props;
+        return (
+            <div>
       <MuiThemeProvider>
        <AppNav
-          menuState={this.props.inClass}
-          height={this.state.height}
-          onTodoAdd={height =>
-            this.props.actions.addClass(height)
-          }
-          onTodoRemove={height =>
-            this.props.actions.removeClass(height)
-          }
-           />   
+            menuState={this.props.inClass}
+            height={this.state.height}
+            onTodoAdd={height => this.props.actions.addClass(height)
+            }
+            onTodoRemove={height => this.props.actions.removeClass(height)
+            }
+            />   
       </MuiThemeProvider>
-      <div id="leftMenu" className={liClasses} ref="leftMenu"  style={{height:this.props.height}}>
+      <div id="leftMenu" className={liClasses} ref="leftMenu"  style={{
+                height: this.props.height
+            }}>
         <ul id="content" ref="content">
-         <li><a onClick={(e) => this.handleClick(e,'/')}>MENU</a></li>
-         <li><a onClick={(e) => this.handleClick(e,'/content')}>內容</a></li>
-         <li><a onClick={(e) => this.handleClick(e,'/canvas')}>CanvasGame</a></li>
-         <li><a onClick={(e) => this.handleClick(e,'/d3')}>d3View</a></li>
-         <li><a onClick={(e) => this.handleClick(e,'/memList')}>fetchJsonData</a></li>
+         <li><a onClick={(e) => this.handleClick(e, '/')}>MENU</a></li>
+         <li><a onClick={(e) => this.handleClick(e, '/content')}>內容</a></li>
+         <li><a onClick={(e) => this.handleClick(e, '/canvas')}>CanvasGame</a></li>
+         <li><a onClick={(e) => this.handleClick(e, '/d3')}>d3View</a></li>
+         <li><a onClick={(e) => this.handleClick(e, '/memList')}>fetchJsonData</a></li>
         </ul>    
       </div>
       <TopBar />
-      <div style={{position:'relative'}}>
+      <div style={{
+                position: 'relative'
+            }}>
          {this.props.children}
          <Footer />
          <div className="loading" style={loadingStyle}>     	
@@ -150,30 +139,30 @@ class App extends Component {
 		       <div style={LoadImage}><img src={loadingImg} alt="" /></div>      
       </div>
     </div>
-		);
-	}
+        );
+    }
 }
 
 //只專注特定屬性變化
 function mapStateToProps(state) {
-	return {
-		inClass: state.menuSet.inClass,
-		height: state.menuSet.height,
-		unmount: state.canvasUnmount.unmount,
-	}
+    return {
+        inClass: state.menuSet.inClass,
+        height: state.menuSet.height,
+        unmount: state.canvasUnmount.unmount,
+    }
 }
 
 //mapDispatchToProps的作用是把store中的dispatch方法注入给组件 == store.dispatch
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators({
-			addClass: addTodo,
-			removeClass: removeTodo,
-			Unmount: Unmount,
-			Didmount: Didmount,
-		}, dispatch)
+    return {
+        actions: bindActionCreators({
+            addClass: addTodo,
+            removeClass: removeTodo,
+            Unmount: Unmount,
+            Didmount: Didmount,
+        }, dispatch)
 
-	}
+    }
 }
 
 
